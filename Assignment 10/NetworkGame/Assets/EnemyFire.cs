2310e4a98119f4e6e5270ede51be21910b9ed2a1
hansenbeast <wangzehao1998@126.com>
2018-06-26 23:03:00 +0800

    Assignment 10

---
+++ Assignment 10/NetworkGame/Assets/EnemyFire.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+using UnityEngine;
+
+public class EnemyFire : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+	
+	private float time = 0;
+
+	[Command]
+  void CmdFire()
+  {
+     // This [Command] code is run on the server!
+
+     // create the bullet object locally
+     var bullet1 = (GameObject)Instantiate(
+          bulletPrefab,
+          transform.position - transform.forward,
+          Quaternion.identity);
+
+     bullet1.GetComponent<Rigidbody>().velocity = -transform.forward*4;
+
+     // var bullet2 = (GameObject)Instantiate(
+     //      bulletPrefab,
+     //      transform.position + transform.forward,
+     //      Quaternion.identity);
+
+     // bullet2.GetComponent<Rigidbody>().velocity = -transform.forward*4;
+     
+     // spawn the bullet on the clients
+     NetworkServer.Spawn(bullet1);
+     // NetworkServer.Spawn(bullet2);
+     // when the bullet is destroyed on the server it will automaticaly be destroyed on clients
+     Destroy(bullet1, 2.0f);
+     // Destroy(bullet2, 2.0f);
+  }
+
+	
+	// Update is called once per frame
+	void Update () {
+		time+=Time.deltaTime;
+		if(time>1){
+        	// Command function is called from the client, but invoked on the server
+        	CmdFire();
+        	time = 0;
+    }
+	}
+}
--- README.md
+++ README.md
@@ -99,3 +99,24 @@ Github pages:https://hansenbeast.github.io/post/2018-06-19-unity3d-lecture-9/
 
 视频地址：https://pan.baidu.com/s/1--Ql7bwLUCtVjY8m_go9uQ
 
+
+
+## Assignment 9
+
+- Assets包括md插图
+- Priest and Devid AI是牧师与魔鬼AI版的项目库
+- README说明文档
+
+Github pages:https://hansenbeast.github.io/post/2018-06-19-unity3d-lecture-9/
+
+视频地址：https://pan.baidu.com/s/1--Ql7bwLUCtVjY8m_go9uQ
+
+
+
+## Assignment 10
+
+- NetworkGame是多人联网游戏的项目库
+
+Github pages:https://hansenbeast.github.io/post/2018-06-26-unity3d-lecture-10/
+
+视频地址：https://pan.baidu.com/s/1BCIGL95NYnki5WnvRaGWxg
